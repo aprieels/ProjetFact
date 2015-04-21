adc5c6a907d41adfda42056b197a2256370f1a0b
Brunoob <brunodegomme@hotmail.com>
2015-04-21 15:44:16 +0200

    ajout lecture fichier et buffer des prod-cons

--- fact.c
+++ fact.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <pthread.h>
-#include <curl\curl.h>
+//#include <curl/curl.h>
 #include <endian.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int getmaxthreads(int argc, char * argv[]);
 void * consumer(void * arg);
 int lecture(int argc, char * argv[]);
+void addtobuffer(uint64_t nombre);
+uint64_t readfrombuffer();
+
+#define BSIZE 100 //a faire varier
+uint64_t buffer [BSIZE];
+int index=0;
+pthread_mutex_t buffermutex;	
+sem_t empty;
+sem_t full;
+
+
 
 int main(int argc, char * argv[]){
 	//récupérer le nombre max de threads passé en argument
@@ -14,14 +30,18 @@ int main(int argc, char * argv[]){
 printf("maxthr=%i\n", nthr);
 	if(nthr==0)
 		return EXIT_FAILURE;
+	
+	//initialisation du buffer à partager entre producers et consumers
+	pthread_mutex_init( &buffermutex, NULL);	
+	sem_init(&empty, 0, BSIZE);
+	sem_init(&full, 0, 0);
 
 
-
-	//lance les threads consumer pour lire les nombres
+	//lance les threads consumer pour factoriser les nombres
 	pthread_t threads [nthr];
 	int i;
 	for(i=0; i<nthr; i++){
-		pthread_create(&(threads[i]), NULL, &consumer, ***); //consumer?
+		pthread_create(&(threads[i]), NULL, &consumer, NULL); //consumer?
 	}
 
 
@@ -30,6 +50,7 @@ printf("maxthr=%i\n", nthr);
 	if(e!=0)
 		return EXIT_FAILURE;
 	
+
 	//la lecture des fichiers est terminée
 	//récupère les listes chainées de chaque thread
 	void * retval [nthr]; //malloc?bof, void?
@@ -62,19 +83,62 @@ void * consumer(void * arg){
 }
 
 int lecture(int argc, char * argv[]){
-	int isstdin=0; // vaudra 1 s'il faut lire les arguments passés en standard in
+	int isstdin=0; //vaudra 1 s'il faut lire les arguments passés en standard in
+	int descr;
+	int e;
+	uint64_t nombre;
 	int i;
 	for(i=1; i<argc; i++){
 		if(strcmp(argv[i], "-stdin")==0)
 			isstdin=1;
 		else if (strcmp(argv[i],"file")==0){
-			//ouvrir fichier de nom argv[i+1]
+			//ouvrir le fichier
+			if((descr=open(argv[i+1], O_RDONLY))<0){
+				perror("open");
+				return EXIT_FAILURE;
+			}
+			//lire le fichier
+			while( (e=read(descr, &nombre, sizeof(uint64_t))) != 0){
+				if(e<0){
+					perror("read");
+					return EXIT_FAILURE;
+				}
+				nombre=be64toh(nombre);//nombre transformé en représentation locale
+				addtobuffer(nombre);
+
+			}
+			//fermer le fichier
+			if(close(descr)!=0){
+				perror("close");
+				return EXIT_FAILURE;
+			}
 		}
 		else if (strncmp(argv[i],"http://",7)==0){
 			//lire le fichier depuis le reseau
 		}
 	}
-	if(isstdin==1){ //s'il faut lire des nombres de la ligne de commande
+	if(isstdin==1){//s'il faut lire des nombres de la ligne de commande
 
 	}
 }
+
+void addtobuffer(uint64_t nombre){
+	sem_wait(&empty);
+	pthread_mutex_lock(&buffermutex);
+		buffer[index]=nombre;
+		index++;
+	pthread_mutex_unlock(&buffermutex);
+	sem_post(&full);	
+}
+
+uint64_t readfrombuffer(){
+	uint64_t nombre;
+	sem_wait(&full);
+	pthread_mutex_lock(&buffermutex);
+		nombre=buffer[index];
+		index--;
+	pthread_mutex_unlock(&buffermutex);
+	sem_post(&empty);
+	return nombre;
+}
+
